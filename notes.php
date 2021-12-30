473fa990909c041dd7059b18954d04a50a5545f5
Marcus Overstreet <tkogiah@gmail.com>
2021-12-30 11:24:04 -0800

    more notes

--- notes.php
+++ notes.php
@@ -18,7 +18,7 @@
         
         echo "<br> Hello $string $integer and $float";
 
-
+        //section 1
         echo '<br><br>';
         echo 'working with strings';
         echo '<br>';
@@ -37,6 +37,7 @@
         echo '<br>';
         echo substr($string, 1, 3); //will pull a 3 char substring starting at index 1.
 
+        //section 2
         echo '<br><br>';
         echo 'working with numbers';
         echo '<br>';
@@ -78,7 +79,9 @@
         echo floor(3.9); //always rounds decimals down
     ?>
 
+    
     <?php 
+    //section 3
         echo '<br><br>';
         echo 'working with user input';
     ?>
@@ -93,6 +96,7 @@
     <?php echo $_GET["name"]?>
 
     <?php 
+    //section 4
         echo '<br><br>';
         echo 'building a basic calculator';
     ?>
@@ -111,7 +115,98 @@
         
     ?>
 
+    <?php
+    //section 5
+        echo '<br><br>';
+        echo 'building a mad libs game';
+    ?>
 
+    <form action='notes.php' method='get'>
+        color:<input type="text" name="color"><br>
+        plural noun<input type="text" name="pluralNoun"><br>
+        celebrity<input type="text" name="celebrity"><br>
+        <input type="submit">
+    </form>    
+
+
+    <?php
+        $color = $_GET['color'];
+        $pluralNoun = $_GET['pluralNoun'];
+        $celebrity = $_GET['celebrity'];
+        echo '<br>';
+        echo "roses are $color <br>";
+        echo "$pluralNoun are blue <br>";
+        echo "I love $celebrity <br>";
+    ?>
+    
+    <?php
+    //section 6
+        echo '<br><br>';
+        echo 'working with URL parameters';
+    ?>
+
+    <form action='notes.php' method='get'>
+        name2:<input type="text" name="nameTwo"><br>
+        <!-- didn't use 'name' becasue i used it above and the name attribute needs to be unique -->
+        <input type="submit">
+    </form>
+    <br>
+    
+    <?php
+        echo $_GET['nameTwo'];
+        echo '<br>CHECK NOTES CODE<br>';
+        // echo "if you modify the url you can create a variable if you would like<br>";
+        // echo "try changing \'http://localhost:4000/notes.php?nameTwo=john'\ <br>";
+        // echo "to<br>";
+        // echo "http://localhost:4000/notes.php?age=70 <br> and then change the '$_GET['nameTwo']' variable to be '$_GET['age']'";
+    ?>
+
+    <?php
+    //section 7
+        echo '<br><br>';
+        echo 'post vs get';
+        echo '<br>';
+        echo "replace GET with POST in previous activities to hide info in the URL";
+    ?>
+
+    <?php
+    //section 8 
+        echo '<br><br>';
+        echo 'working with arrays <br>';
+        $friends = array('kevin', 'karen','oscar', 'jim');
+        echo "$friends[0]<br>";
+        echo "$friends[1]<br>";
+        echo "$friends[2]<br>";
+        echo "$friends[3]<br>";
+        $friends[1] = 'dwight';
+        echo "$friends[1]<br>";
+        echo count($friends);
+        echo "<br>"
+    ?>
+
+    <form action="notes.php" method="post">
+        apples<input type="checkbox" name="fruits[]" value="apples"><br>
+        oranges<input type="checkbox" name="fruits[]" value="oranges"><br>
+        bananas<input type="checkbox" name="fruits[]" value="bananas"><br>
+        pineapple<input type="checkbox" name="fruits[]" value="pineapple"><br>
+        grapes<input type="checkbox" name="fruits[]" value="grapes"><br>
+        <input type="submit">
+    </form>
+
+    <?php 
+        $fruits = $_POST["fruits"];
+        echo $fruits[1]
+    ?>
+
+    <?php 
+    //section 9
+        echo "<br><br>";
+        echo "working with associated arrays<br>";
+    ?>
+    <form action="notes.php" method="post">
+        <input type="text" name="student">
+        <input type="submit">
+    </form>
 
 </body>
 </html>
